3f7c1f2878a2de28f52db570a0dc2a5078587435
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-10-01 14:10:12 -0300

    test: Add unit tests for MealDetailsViewModel

---
+++ PocketChefTests/Features/MealDetails/MealDetailsViewModelTests.swift
@@ -0,0 +1,87 @@
+//
+//  MealDetailsViewModelTests.swift
+//  PocketChefTests
+//
+//  Created by Rodrigo Cerqueira Reis on 01/10/25.
+//
+
+import Foundation
+import XCTest
+import Combine
+@testable import PocketChef
+
+@MainActor
+final class MealDetailsViewModelTests: XCTestCase {
+
+    // MARK: - Properties
+    
+    private var sut: MealDetailsViewModel!
+    private var mockNetworkService: MockNetworkService!
+    private var mockMealSummary: Meal!
+    private var cancellables: Set<AnyCancellable>!
+
+    // MARK: - Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        mockMealSummary = Meal(id: "52772", name: "Teriyaki Chicken Casserole", thumbnailURLString: "")
+        mockNetworkService = MockNetworkService()
+        sut = MealDetailsViewModel(meal: mockMealSummary, networkService: mockNetworkService)
+        cancellables = []
+    }
+
+    override func tearDown() {
+        sut = nil
+        mockNetworkService = nil
+        mockMealSummary = nil
+        cancellables = nil
+        super.tearDown()
+    }
+
+    // MARK: - Test Cases
+    
+    func testFetchDetails_WhenRequestSucceeds_ShouldPublishDetails() async {
+        let mockDetails = PocketChef.MealDetails(id: "52772", name: "Teriyaki Chicken Casserole", instructions: "...", thumbnailURLString: nil, ingredients: [])
+        let mockResponse = MealDetailsResponse(meals: [mockDetails])
+        mockNetworkService.mockResult = .success(mockResponse)
+        
+        let expectation = self.expectation(description: "Publishes meal details")
+        var receivedDetails: PocketChef.MealDetails?
+        
+        sut.detailsPublisher
+            .sink { details in
+                receivedDetails = details
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        await sut.fetchDetails()
+        
+        await fulfillment(of: [expectation], timeout: 1.0)
+        
+        XCTAssertNotNil(receivedDetails)
+        XCTAssertEqual(receivedDetails?.id, "52772")
+    }
+    
+    func testFetchDetails_WhenRequestFails_ShouldPublishError() async {
+        let mockError = NetworkError.invalidURL
+        mockNetworkService.mockResult = .failure(mockError)
+        
+        let expectation = self.expectation(description: "Publishes an error message")
+        var receivedError: String?
+
+        sut.errorPublisher
+            .sink { errorMessage in
+                receivedError = errorMessage
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+
+        await sut.fetchDetails()
+        
+        await fulfillment(of: [expectation], timeout: 1.0)
+
+        XCTAssertNotNil(receivedError, "Should receive an error message.")
+        XCTAssertEqual(receivedError, mockError.localizedDescription)
+    }
+}
